84fab3bdb79ae27466de743e75bfd081c8238da9
odyslam.eth <odyslam@gmail.com>
2021-12-15 15:09:39 +0300

    project skeleton (#1)

---
+++ src/test/utils/WorkStreamTest.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import {Hevm} from "./Hevm.sol";
+import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+
+contract WorkStreamTest is DSTestPlus {}
