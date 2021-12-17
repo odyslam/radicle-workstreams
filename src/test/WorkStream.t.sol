81acb934fe168332065fbf70ef2331802103073d
Odysseas Lamtzidis <odyslam@gmail.com>
2021-12-17 23:38:37 +0300

    chore: test skeleton

--- src/test/WorkStream.t.sol
+++ src/test/WorkStream.t.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
+
+
+contract GenericTests is WorkStreamTest{
+
+    function communityMemberCreateWorkStream() public {
+
+
+
+    }
+
+
+
+}
--- src/test/utils/WorkStreamTest.sol
+++ src/test/utils/WorkStreamTest.sol
@@ -3,5 +3,26 @@ pragma solidity ^0.8.10;
 
 import {Hevm} from "./Hevm.sol";
 import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
+import {IOrgV1} from "../../IOrgV1.sol";
 
-contract WorkStreamTest is DSTestPlus {}
+
+contract user is DSTestPlus{
+
+
+
+    function createWorkStream()
+
+}
+
+contract WorkStreamTest is DSTestPlus{
+
+    IOrgV1 exampleOrg;
+
+
+    function setUp() public{
+        address exampleOrgAddress = 0xe22450214b02C2416481aC2d3Be51536f7bb1fFf;
+        exampleOrg IOrgV1(exampleOrgAddress);
+    }
+
+}
+
