cb2e42d176c29e2ec223e9149938adea57fe5d0d
Odysseas Lamtzidis <odyslam@gmail.com>
2021-12-17 23:39:30 +0300

    chore: add OrgV1 interface

--- src/IOrgV1.sol
+++ src/IOrgV1.sol
@@ -1,2 +1,19 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
+
+
+interface IOrgV1 {
+
+    address public owner;
+
+    mapping (bytes32 => Anchor) public anchors;
+
+    struct Anchor
+    {
+    // A tag that can be used to discriminate between anchor types.
+    uint32 tag;
+    // The hash being anchored in multihash format.
+    bytes multihash;
+    }
+
+}
