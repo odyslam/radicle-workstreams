c90e688880b736d4d3574426428c2a96d73aca3f
Odysseas Lamtzidis <odyslam@gmail.com>
2021-12-24 20:19:31 +0300

    chore: lint, remove unecessery interfaces

--- src/IDAIDripsHub.sol
+++ src/IDAIDripsHub.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
 
-interface IDAIDripsHub{
-
+interface IDripsHub {
     struct PermitArgs {
         uint256 nonce;
         uint256 expiry;
--- src/test/utils/WorkStreamTest.sol
+++ src/test/utils/WorkStreamTest.sol
@@ -5,16 +5,13 @@ import {Hevm} from "./Hevm.sol";
 import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 import {IOrgV1} from "../../IOrgV1.sol";
 
+contract user is DSTestPlus {}
 
-contract user is DSTestPlus{}
-
-contract WorkStreamTest is DSTestPlus{
-
+contract WorkStreamTest is DSTestPlus {
     IOrgV1 exampleOrg;
-    function setUp() public{
+
+    function setUp() public {
         address exampleOrgAddress = 0xe22450214b02C2416481aC2d3Be51536f7bb1fFf;
-        exampleOrg  = IOrgV1(exampleOrgAddress);
+        exampleOrg = IOrgV1(exampleOrgAddress);
     }
-
 }
-
